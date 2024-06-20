4f0d104df122625f3e7ff12e8880870c76782a3a
Elsaid Wael <alghnam906@gmail.com>
2024-06-20 23:28:52 +0300

    basic set up and some code for the project

---
+++ script.js
@@ -0,0 +1,8 @@
+const grid = document.querySelector('#grid');
+for (let i = 0; i < 16 * 16; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    grid.appendChild(square);
+
+}
+
